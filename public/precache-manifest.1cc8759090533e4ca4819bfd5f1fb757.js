1356ed792f6596c1782f65eabf8750f78bb0f3aa
Valdas <valdas.trakumas@ibm.com>
2019-10-11 10:30:12 +0300

    removed some links added new build

---
+++ public/precache-manifest.1cc8759090533e4ca4819bfd5f1fb757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0992587b652474f3ce0e",
+    "url": "/static/js/main.0992587b.chunk.js"
+  },
+  {
+    "revision": "176f9a7f7d22492187bf",
+    "url": "/static/js/1.176f9a7f.chunk.js"
+  },
+  {
+    "revision": "0992587b652474f3ce0e",
+    "url": "/static/css/main.57cab1e8.chunk.css"
+  },
+  {
+    "revision": "176f9a7f7d22492187bf",
+    "url": "/static/css/1.687416c0.chunk.css"
+  },
+  {
+    "revision": "6fffea08a9e79cb8b46179bd66a64123",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0942299afd32e9aaaffe1c7996adc6c6.js"
+  "/precache-manifest.1cc8759090533e4ca4819bfd5f1fb757.js"
 );
 
 workbox.clientsClaim();
